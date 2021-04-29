d3a67a6df4c6870b25ca075c2b4b3ba5908420fa
Bobi <bobissss84@gmail.com>
2021-04-29 21:56:43 +0300

    rdy

--- scripts/init.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    let showReposButton = document.getElementById('nav-projects-button');
-    showReposButton.addEventListener('click', function () {
-        alert('Repositories to be added!');
-    });
-});
